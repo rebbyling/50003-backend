c8e23afef89c80f512734effddc36149c4c48d03
LI MEIXUAN/Stephanie <67006512+meixuan-stephanie@users.noreply.github.com>
2021-03-19 23:05:29 +0800

    Add files via upload

---
+++ SingHealth/accounts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('', views.home, name="home"),
+    path('tenants/', views.tenants, name="tenants"),
+    path('staff/<str:pk>/', views.staff, name="staff"),
+    path('create_audit/', views.createAudit, name="create_audit"),
+    path('update_audit/', views.updateAudit, name="update_audit"),
+
+]
